dedd6556cf74a18129e7d68d18814b7361d19409
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-05-15 10:09:44 +0800

    Add files via upload

---
+++ D11416214_W7/D11416214_W7/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public interface IName
+{
+
+}
+public class Person:IName
+{
+	public void GetName
+	{
+		
+	}
+	
+}
---
+++ D11416214_W7/D11416214_W7/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace D11416214_W7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IName p1=new Person();
+            Console.WriteLine(p1.GetName());
+        }
+    }
+}
